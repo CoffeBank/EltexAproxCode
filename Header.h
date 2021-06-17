d8ce07708243b81a81c14a82a857b860601de547
CoffeBank <44469043+CoffeBank@users.noreply.github.com>
2021-06-18 02:52:21 +1000

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,23 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+#define IDM_EXIT 100
+#define IDM_ABOUT 101
+#define IDM_COLORGRAPH 102
+#define IDM_OPEN 103
+#define IDM_SHOW 104
+#define IDM_COLORPOINTS 106
+#define IDM_OPTIONS 107
+#endif RESOURCE_H
+
+#ifndef IDC_STATIC 
+#define IDC_STATIC -1 
+#define IDC_EDIT1 101 
+#define IDC_EDIT2 102 
+#define IDC_EDIT3 103 
+#define IDC_EDIT4 104 
+#define IDC_EDIT5 105 
+#define IDC_EDIT6 106 
+#define IDC_A 107
+#define IDC_b 108
+#define IDC_B 109
+#endif
